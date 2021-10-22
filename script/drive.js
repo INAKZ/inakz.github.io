291c51f4d1d645c9ce1386597e357294b04865b8
inakz <45687694+INAKZ@users.noreply.github.com>
2021-10-22 19:56:03 +0900

    Update drive.js

--- script/drive.js
+++ script/drive.js
@@ -10,10 +10,10 @@ function url_convert(){
 	target.href = "";
 	
 	urlTmp = urlInput.split('/');
-	if(urlTmp[5] != undefined && urlTmp[2] == "drive.google.com"){
+	if(urlTmp[0] == "https:" && urlTmp[2] == "drive.google.com"){
 		urlOutput = "https://drive.google.com/uc?id=" + urlTmp[5] + "&export=download";
 		target.href = (urlOutput);
-	} else if(urlTmp[5] == undefined){
+	} else if(urlTmp[0] != "https:"){
 		urlOutput = "";
 	} else {
 		urlOutput = "Error:URLがGoogleDriveのものではないか、仕様変更で使えません。";
