8fceabdd2bda80fc95e116433319853537d69f89
inakz <45687694+INAKZ@users.noreply.github.com>
2021-11-16 22:37:02 +0900

    update tools

--- js/urlutil.js
+++ js/urlutil.js
@@ -1,7 +1,19 @@
 "use strict";
 
+//出力をクリップボードにコピー
+function CopyToClipboard(){
+	let target = Function("return document.url_form.url_out.value;")();
+	navigator.clipboard.writeText(target).then(function() {
+		//成功したら選択
+		document.url_form.url_out.select();
+	}, function() {
+		//失敗したらアラート
+		alert("コピーに失敗しました");
+	});
+}
+
 //URL変換
-function url_convert(){
+function UrlConvert(){
 	const M_GEN = '0', M_DRI = '1', M_PIP = '2', M_INV = '3', M_NIT = '4', M_NITS = '5';
 	const invIns = ["invidious.namazso.eu", "invidio.xamh.de", "vid.puffyan.us", "youtube.076.ne.jp", "yewtu.be", "invidious-us.kavin.rocks", "inv.riverside.rocks", "vid.mint.lgbt", "invidious-jp.kavin.rocks", "invidious.snopyta.org", "invidious.kavin.rocks", "yt.artemislena.eu", "invidious.osi.kr"];
 	const nitIns = ["nitter.net", "nitter.42l.fr", "nitter.pussthecat.org", "nitter.nixnet.services", "nitter.fdn.fr", "nitter.1d4.us", "nitter.kavin.rocks", "nitter.vxempire.xyz", "nitter.unixfox.eu", "nitter.unixfox.eu", "nitter.eu", "nitter.namazso.eu", "nitter.mailstation.de", "nitter.actionsack.com", "birdsite.xanny.family", "nitter.hu", "nitter.exonip.de", "twitr.gq", "nitter.moomoo.me", "bird.trom.tf", "nitter.it", "twitter.censors.us", "nitter.grimneko.de", "nitter.koyu.space", "nitter.alefvanoon.xyz", "nitter.ir", "nitter.autarkic.org", "n.0x0.st", "n.hyperborea.cloud", "nitter.ca", "twitter.076.ne.jp", "lu-nitter.resolv.ee", "is-nitter.resolv.ee", "cy-nitter.resolv.ee", "nitter.mstdn.social", "nitter.fly.dev", "notabird.site", "nitter-jp.kavin.rocks"];
